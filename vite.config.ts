d803759926bd1dba923de6688c3d344bbcc65d6c
Paul Handorff <handorff.paul@gmail.com>
2026-02-02 07:24:56 -0500

    rename project

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
 
 export default defineConfig(({ command }) => ({
-  base: command === "build" ? "/moonrise-svg/" : "/",
+  base: command === "build" ? "/plotter-art/" : "/",
 }));
